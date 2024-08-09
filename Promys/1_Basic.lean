d7c4720ed7865da1e89871fc226e56101ba6d01a
Jiahua Chen <33486899+jchen@users.noreply.github.com>
2024-08-09 14:27:39 +0000

    Changes to 1

--- Promys/1_Basic.lean
+++ Promys/1_Basic.lean
@@ -1,39 +1,72 @@
 import Mathlib.Data.Nat.Defs
+import Mathlib.Data.Nat.Prime.Defs
 
 /-
-Everything in Lean has a type! We can check its type using #type
+EVERYTHING in Lean has a type! We can check its type using #type
 -/
 
+-- Numbers
+#check 1
+#check 0
+#check 2.3
+
+-- Cartesian Coordinates
+#check (1, 2)
+
+-- Sets
+#check {x : ℤ | ∃ k, x = 2 * k}
+
+-- We can define functions on numbers
+def f x := -1 * x
+#check f
+-- A (ℤ → ℤ) means it takes an integer and returns an integer.
+
+-- We might write it like this
+#check (λ x ↦ -1 * x)
+
+-- Types must match! What if we tried to pass it a Cartesian coordinate? (type error)
+def p := (1, 2)
+#check f p
+
+-- We can have multiple arguments
+def g x y := x * y + 4
+#check g
+
+-- Types have types too! Types have type 'Type'.
 #check ℕ
 #check ℤ
 
-#check Empty
-#check Unit
-#check Bool
+-- Wait...
+#check Type
+#check Type 1
 
-#check ℕ → ℤ
-#check ℤ → ℕ
-#check Bool → ℕ → ℤ
-#check (Bool → ℕ) → ℤ
-#check ℕ → (Bool → ℕ) → ℤ
-
-#check fun x : ℕ ↦ x
-#check fun f : ℕ → ℕ ↦ fun g : ℕ → ℕ ↦ fun h : ℕ → ℕ ↦
-  fun x : ℕ ↦ h (g (f x))
-#check fun (f g h : ℕ → ℕ) (x : ℕ) ↦ h (g (f x))
-
-/-
-What about the type of statements?
--/
-
+-- What about the type of statements?
+-- Statements are typed 'Prop' for proposition.
 #check 1 < 2
+#check ∀ (x : ℤ), ∃ y, y ∣ x
+#check True
+#check False
 
--- Type checking is not evaluation!
+-- Type checking is not evaluation! Incorrect statements can exist.
 #check 2 < 1
+#check 2 < 1 → 1 < 0
 
-/-
-Backwards Proofs
--/
+-- Something can have a type that is the proposition. That is a _proof_ of said proposition.
+
+def exists_diophantine_solutions x y := ∃ a b, a * x + b * y = 1
+#check exists_diophantine_solutions
+
+axiom bezout {x y} : Nat.gcd x y = 1 → exists_diophantine_solutions x y
+#check bezout
+
+-- If I have a proof that Nat.gcd x y = 1
+theorem proof_of_5_7_coprime : Nat.gcd 5 7 = 1 := by trivial
+#check proof_of_5_7_coprime
+
+-- I can apply it to Bezout's Lemma to get a proof that there exists solutions to diophantine equation 5a + 7b = 1
+#check bezout proof_of_5_7_coprime
+
+-- Generally, we can't prove things using 'trivial'. We generally prove things in Proof mode. A view pops up on the right with our current hypotheses at all times and the goal of the statement we are trying to prove
 
 theorem prop_comp (a b c : Prop) (hab : a → b) (hbc : b → c) :
   a → c :=
